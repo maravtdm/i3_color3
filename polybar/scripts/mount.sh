3fad1fc298d7c8215338e4ab754a7520b5ec7151
marav <maravtdm@users.noreply.github.com>
2018-03-22 22:56:17 +0100

    Update mount.sh

--- polybar/scripts/mount.sh
+++ polybar/scripts/mount.sh
@@ -2,9 +2,9 @@
 
 label_prefix=""
 label_umount=""
-for dev in 'fedora' 'ssh' 'yang'
+for dev in 'mountpoint1' 'mountpoint2' 'mountpoint3'
 do
-	mount | grep /mnt/$dev >/dev/null 2>&1
+	mount | grep /mnt/$dev >/dev/null 2>&1  # if your mountpoints are in /mnt
 	mount=$?
 	if [ "$mount" == "0" ]; then
 #		echo -n "  $label"
