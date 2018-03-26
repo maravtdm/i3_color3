14578bb56792a6bc06495264935c746aa27df5c0
marav <maravtdm@users.noreply.github.com>
2018-03-26 14:36:27 +0200

    Update wifi.sh

--- polybar/scripts/wifi.sh
+++ polybar/scripts/wifi.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+#######################
+### Polybar config ####
+#######################
+# [module/wifi]
+# type = custom/script
+# exec = $HOME/.config/polybar/scripts/wifi.sh
+# format-prefix = " "
+# format-prefix-foreground = #1793d1 #i.e.
+### to have all states visible during the connexion, specify an interval < 1
+# interval = 0.5 
+
+
 state=$(nmcli dev sh wlp1s0 | awk '/GENERAL.STATE:/ {print $2}')
 essid=$(nmcli dev sh wlp1s0 | awk '/GENERAL.CONNECTION:/ {print $2}')
 label_deconnected=" "
@@ -17,4 +29,3 @@ case $state in
 	echo "$essid"
 	;;
 esac
-
